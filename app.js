52b88eaea3d6267f273a9ee4db70f7263e042ec2
unknown <yyreyes@gmail.com>
2019-06-01 11:21:34 -0400

    Files for thesections  about and contact

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.get("/", function(req, res){
   res.render("home" , {homeStartingContent:homeStartingContent});
 });
 
+app.get("/about", function(req, res){
+  res.render("about" , {aboutContent:aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact",{contactContent:contactContent});
+});
 
 
 
