dccc5db51d5c20414ad615d4f1bc0ce2d485a087
unknown <yyreyes@gmail.com>
2019-06-08 09:37:17 -0400

    updates on the app.js

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-  res.render("home" , {homeStartingContent:homeStartingContent});
-  console.log(posts);
+  res.render("home" , {
+  homeStartingContent:homeStartingContent,
+  posts: posts
+});
+
 });
 
 app.get("/about", function(req, res){
